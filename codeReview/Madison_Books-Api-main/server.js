d7089f541cea602b57b66264c6660c2d42168444
steve_D <stephenCdoherty@gmail.com>
2023-11-03 00:51:58 -0500

    code review, fixes

--- codeReview/Madison_Books-Api-main/server.js
+++ codeReview/Madison_Books-Api-main/server.js
@@ -4,27 +4,31 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
 
-// mongoose
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to mongo on: ', process.env.MONGO_URI)
-})
-
 //middleware
 require('dotenv').config()
 const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-// root 
+// mongoose
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
+  }
+)
+
+// root
 app.get('/', (req, res) => {
-    res.send('Hello and Welcome!')
+  res.send('Hello and Welcome!')
 })
 
-// books 
+// books
 const booksController = require('./controllers/books_controller')
 app.use('/books', booksController)
 
 // listen
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
+  console.log(`Listening on port: ${PORT}`)
 })
